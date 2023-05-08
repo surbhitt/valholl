a504b304bb1dd1a237acf006313e76d5570106d1
surbhitt <surbhit.thakur@gmail.com>
2023-05-08 13:57:16 +0530

    added font and some formatting

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,18 +4,37 @@ import "./styles.css"
 export default function Header () {
     return (
         <div>
-            <div className="bkg-img-head w-full flex items-center">
-                <div className="flex my-auto mx-auto">
+            <div className="bkg-img-head flex w-full items-center">
+                <div className="flex mx-auto items-center">
                     <div className="flex flex-col">
-                        <h1 className="xs:text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-6xl text-white font-semibold">Surbhit Thakur</h1>
-                        <div className="flex mt-2">
-                            <p className="xs:text-xs text-sm text-white mr-2">surbhit.thakur@gmail.com,</p>
-                            <p className="xs:text-xs text-sm text-white">9868123940</p>
+                        <h1 
+                            className="name xs:text-lg sm:text-xl md:text-2xl lg:text-4xl xl:text-6xl text-white font-semibold"
+                        >
+                            Surbhit Thakur
+                        </h1>
+                        <div className="contact flex mt">
+                            <p className="xs:text-xs text-md text-white mr-2">surbhit.thakur@gmail.com,</p>
+                            <p className="xs:text-xs text-md text-white">9868123940</p>
                         </div>
-                        <div className="mt-5 mx-auto items-center">
-                            <button className="xs:text-xs text-sm text-white border border-white mr-10">git</button>
-                            <button className="xs:text-xs text-sm text-white border border-white mr-10">linkedin</button>
-                            <button className="xs:text-xs text-sm text-white border border-white mr-10">leetcode</button>
+                        <div className="socials flex mt-3 ml-2 items-center">
+                            <a 
+                                className="xs:text-xs text-sm text-white border border-white mr-10"
+                                href="https://github.com/surbhitt/"
+                            >
+                                <img src="assets/github.png" className="h-10" alt="Github" />
+                            </a>
+                            <a 
+                                className="xs:text-xs text-sm text-white border border-white mr-10"
+                                href="https://linkedin.com/in/surbhitthakur/"
+                            >
+                                <img src="assets/linkedin.png" className="h-10" alt="Linkedin" />
+                            </a>
+                            <a 
+                                className="xs:text-xs text-sm text-white border border-white"
+                                href="https://leetcode.com/savas_karanli/"
+                            >
+                                <img src="assets/code.png" className="h-10" alt="Leetcode" />
+                            </a>
                         </div>
                     </div>
                     <img src="assets/raven.png" alt="raven" className="h-96"/>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import "./styles.css"
+import "./styles.css";
 
 const Navbar = () => {
-    return (
-        <nav className="bkg-img-nav">
-            <div className="card-container flex justify-evenly mx-auto items-center w-3/5">
-                <button className="card border-4 border-white h-60 w-60 text-white text-2xl font-semibold">Projects</button>
-                <button className="card border-4 border-white h-60 w-60 text-white text-2xl font-semibold">Contact</button>
-                <button className="card border-4 border-white h-60 w-60 text-white text-2xl font-semibold">Tech stack</button>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="bkg-img-nav">
+      <div className="card-container flex justify-evenly mx-auto items-center w-3/5">
+        <button className="card border-2 rounded-md h-20 w-52 text-white text-2xl font-semibold">
+          Projects
+        </button>
+        <button className="card border-2 rounded-md h-20 w-52 text-white text-2xl font-semibold">
+          Contact
+        </button>
+        <button className="card border-2 rounded-md h-20 w-52 text-white text-2xl font-semibold">
+          Tech stack
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
